02f3aa82aa6b9da8ca46b48805378461e2ad9c44
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-01-24 16:58:11 +0100

    feat: Creating project, controllers and routes

---
+++ controllers/product.controller.js
@@ -0,0 +1,25 @@
+
+//get all products
+export const getProducts = async (req, res) => {
+ 
+};
+
+//get product by id
+export const getByIdProduct = async (req, res) => {
+ 
+};
+
+//creating a product
+export const createProduct = async (req, res) => {
+  
+};
+
+//updating product
+export const updateProduct = async (req, res) => {
+ 
+};
+
+//delete product
+export const deleteProduct = async (req, res) => {
+  
+};
