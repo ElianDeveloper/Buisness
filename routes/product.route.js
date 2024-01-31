8123f5291ab949e49af6bd0359fc0066166a6a33
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-01-31 08:48:28 +0100

    feat: add function and route for createProduct

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,11 +2,13 @@ const Router = require("express");
 const {
   getProducts,
   getByIdProduct,
+  createProduct,
 } = require("../controllers/product.controller.js");
 
 const router = Router();
 
 router.get("/products", getProducts);
 router.get("/product/:id", getByIdProduct);
+router.post("/product", createProduct);
 
 module.exports = router;
