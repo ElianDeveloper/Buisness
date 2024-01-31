ceeb57286b90cbc926005a37ef578f0c9f8c70df
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-01-31 09:22:32 +0100

    feat: add function and route for getByIdExpense

--- controllers/expense.controller.js
+++ controllers/expense.controller.js
@@ -10,6 +10,28 @@ const getExpenses = async (req, res) => {
   }
 };
 
+//get expense by id
+const getByIdExpense = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const expense = await Expense.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!expense) {
+      return res.status(404).json({ message: "expense does not exist" });
+    }
+
+    res.json(expense);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getExpenses,
+  getByIdExpense,
 };
--- routes/expense.route.js
+++ routes/expense.route.js
@@ -1,8 +1,12 @@
 const Router = require("express");
-const { getExpenses } = require("../controllers/expense.controller");
+const {
+  getExpenses,
+  getByIdExpense,
+} = require("../controllers/expense.controller");
 
 const router = Router();
 
 router.get("/expenses", getExpenses);
+router.get("/expense/:id", getByIdExpense);
 
 module.exports = router;
