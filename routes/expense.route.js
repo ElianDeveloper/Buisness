e93ba1230575060404dce9b130443c6ae86ab15e
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-01-31 09:36:12 +0100

    feat: add function and route for createExpense

--- routes/expense.route.js
+++ routes/expense.route.js
@@ -2,11 +2,13 @@ const Router = require("express");
 const {
   getExpenses,
   getByIdExpense,
+  createExpense,
 } = require("../controllers/expense.controller");
 
 const router = Router();
 
 router.get("/expenses", getExpenses);
 router.get("/expense/:id", getByIdExpense);
+router.post("/expense", createExpense);
 
 module.exports = router;
