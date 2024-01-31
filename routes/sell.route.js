b2a039597a0e755b3f7da83dfc36972235c02bed
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-01-31 10:09:45 +0100

    feat: add function and route for createSell

--- routes/sell.route.js
+++ routes/sell.route.js
@@ -1,9 +1,14 @@
 const Router = require("express");
-const { getSells, getByIdSell } = require("../controllers/sell.controller");
+const {
+  getSells,
+  getByIdSell,
+  createSell,
+} = require("../controllers/sell.controller");
 
 const router = Router();
 
 router.get("/sells", getSells);
 router.get("/sell/:id", getByIdSell);
+router.post("/sell", createSell);
 
 module.exports = router;
