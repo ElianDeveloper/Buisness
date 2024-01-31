20896e97ee00ecae5cb8bbcb29d01321777c8f37
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-01-31 10:05:10 +0100

    feat: add function and route for getByIdSell

--- controllers/sell.controller.js
+++ controllers/sell.controller.js
@@ -10,8 +10,28 @@ const getSells = async (req, res) => {
   }
 };
 
+//get sell by id
+const getByIdSell = async (req, res) => {
+  try {
+    const { id } = req.params;
 
+    const sell = await Sell.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!sell) {
+      return res.status(404).json({ message: "sell does not exist" });
+    }
+
+    res.json(sell);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   getSells,
+  getByIdSell,
 };
